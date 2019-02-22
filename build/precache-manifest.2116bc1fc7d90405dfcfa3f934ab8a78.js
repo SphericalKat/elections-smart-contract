86f79dfa27ecaaacd31b3da10939fc495090f7df
ATechnoHazard <amolele@gmail.com>
2019-02-23 00:45:17 +0530

    I know this is terrible, but I don't care at this point
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

---
+++ build/precache-manifest.2116bc1fc7d90405dfcfa3f934ab8a78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc017ad07c26e00b1da2",
+    "url": "/static/css/main.e9e9d472.chunk.css"
+  },
+  {
+    "revision": "dc017ad07c26e00b1da2",
+    "url": "/static/js/main.dc017ad0.chunk.js"
+  },
+  {
+    "revision": "5722e84e2c1f8b5e2326",
+    "url": "/static/js/1.5722e84e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2fafbae09863e5cc2add146abf51ef4",
+    "url": "/index.html"
+  }
+];
